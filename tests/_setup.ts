aab03511065f24b2895fe4390ac4ca7022bcbf5e
Clay Risser <email@clayrisser.com>
2021-07-09 19:01:52 -0500

    Added typescript support

---
+++ tests/_setup.ts
@@ -0,0 +1,25 @@
+/**
+ * File: /tests/_setup.ts
+ * Project: react-native-ignore-warnings
+ * File Created: 09-07-2021 18:35:16
+ * Author: Clay Risser <email@clayrisser.com>
+ * -----
+ * Last Modified: 09-07-2021 18:44:02
+ * Modified By: Clay Risser <email@clayrisser.com>
+ * -----
+ * Silicon Hills LLC (c) Copyright 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default null;
---
+++ tests/index.ts
@@ -0,0 +1,7 @@
+describe('react-native-ignore-warnings', () => {
+  it('should work', async () => {
+    expect(true).toBe(true);
+  });
+});
+
+export default null;
